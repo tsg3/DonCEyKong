9fd84f87360b224141b2f88edf16bd456ff2d707
Esteban <estebandcg1999@gmail.com>
2019-06-12 03:21:25 -0600

    Nuevo nivel de escaleras. Restricciones en el movimiento y arreglo de un problema con punteros, que causaba la caída del programa aleatoriamente.

--- GUI/window.c
+++ GUI/window.c
@@ -18,7 +18,8 @@ ALLEGRO_BITMAP* escalera = NULL;
 bool key[4] = { false, false, false, false };
 float nivelPiso = 494;
 float salto = 0;
-int plataforma = 1;
+int subiendo_escalera = 0;
+int saltando = 0;
 
 int initialize(){
     if(!al_init()) {
@@ -66,13 +67,16 @@ int initialize(){
     marioY[0] = createImage("jumpman_caminar","jumpman_sprite_3.png");
     marioY[1] = createImage("jumpman_caminar","jumpman_sprite_4.png");
     marioY[2] = createImage("jumpman_caminar","jumpman_sprite_5.png");
+    marioZ[0] = createImage("jumpman_subiendo","jumpman_subiendo_sprite_1.png");
+    marioZ[1] = createImage("jumpman_subiendo","jumpman_subiendo_sprite_4.png");
     escalera = createImage("estructuras","escalera_sprite_0.png");
     xM = 40;
     yM = nivelPiso;
     al_draw_bitmap(marioX[0], xM, yM, 0);
 
 
-    if(!image || !image2 || !martillo || !gasolina) {
+    if(!image || !image2 || !martillo || !gasolina || !escalera || !marioX[0] || !marioX[1]
+    || !marioX[2] || !marioY[0] || !marioY[1] || !marioY[2] || !marioZ[0] || !marioZ[1]) {
         al_show_native_message_box(display, "Error", "Error", "Failed to load image!",
                                    NULL, ALLEGRO_MESSAGEBOX_ERROR);
         al_destroy_display(display);
@@ -100,12 +104,20 @@ void finishExecution(){
     al_destroy_bitmap(image2);
     al_destroy_bitmap(martillo);
     al_destroy_bitmap(gasolina);
+    al_destroy_bitmap(escalera);
+    al_destroy_bitmap(marioX[0]);
+    al_destroy_bitmap(marioX[1]);
+    al_destroy_bitmap(marioX[2]);
+    al_destroy_bitmap(marioY[0]);
+    al_destroy_bitmap(marioY[1]);
+    al_destroy_bitmap(marioY[2]);
     al_destroy_timer(timer);
     al_destroy_event_queue(event_queue);
 }
 
 void reading(){
     int i = 0;
+    int j = 0;
     int* k = &i;
     ALLEGRO_BITMAP** mario = marioX;
     while(1)
@@ -113,29 +125,37 @@ void reading(){
         ALLEGRO_EVENT ev;
         al_wait_for_event(event_queue, &ev);
         if(ev.type == ALLEGRO_EVENT_TIMER) {
-            if(key[KEY_LEFT] && key[KEY_RIGHT]) {
-                continue;
-            }
-            else {
+            if(countKeys()==1) {
                 if (key[KEY_UP] && hay_escaleras(1)) {
                     nivelPiso -= 1;
+                    j++;
+                    mario = marioZ;
+                    k = &j;
                 }
                 if (key[KEY_DOWN] && hay_escaleras(0)) {
                     nivelPiso += 1;
+                    j++;
+                    mario = marioZ;
+                    k = &j;
                 }
-                if (key[KEY_LEFT] && xM >= 4.0) {
+                if (key[KEY_LEFT] && xM >= 4.0 && !subiendo_escalera) {
                     xM -= 2.0;
                     i++;
                     mario = marioY;
+                    k = &i;
                 }
-                if (key[KEY_RIGHT] && xM <= 860.0) {
+                if (key[KEY_RIGHT] && xM <= 860.0 && !subiendo_escalera) {
                     xM += 2.0;
                     i++;
                     mario = marioX;
+                    k = &i;
                 }
                 redraw = true;
-                if (i == 15) {
+                if (i > 14) {
                     i = 0;
+                }
+                if (j > 9){
+                    j = 0;
                 }
             }
         }
@@ -147,21 +167,21 @@ void reading(){
                 case ALLEGRO_KEY_UP:
                     key[KEY_UP] = true;
                     break;
-
                 case ALLEGRO_KEY_DOWN:
+
                     key[KEY_DOWN] = true;
                     break;
-
                 case ALLEGRO_KEY_LEFT:
                     key[KEY_LEFT] = true;
                     break;
-
                 case ALLEGRO_KEY_RIGHT:
                     key[KEY_RIGHT] = true;
                     i++;
                     break;
                 case ALLEGRO_KEY_SPACE:
-                    CreateThread(NULL, 0, saltar, NULL, 0, 0);
+                    if (!subiendo_escalera && !saltando) {
+                        CreateThread(NULL, 0, saltar, NULL, 0, 0);
+                    }
                     break;
             }
         }
@@ -190,7 +210,7 @@ void reading(){
             escaleras(escalera);
             platforms(image);
             yM = nivelPiso - obtenerPiso(xM) - salto;
-            al_draw_bitmap(*(mario+(*(k)/5)), xM, yM, 0);
+            al_draw_bitmap(*(mario+((*k)/5)), xM, yM, 0);
             al_flip_display();
         }
     }
@@ -198,50 +218,78 @@ void reading(){
 
 DWORD WINAPI saltar()
 {
+    saltando = 1;
     int i;
     for(i = 0; i < 11; i++){
         Sleep(40);
         salto = i*3;
+        redraw = true;
     }
     Sleep(100);
     for(; i >= 0; i--){
         Sleep(40);
         salto = i*3;
+        redraw = true;
     }
+    saltando = 0;
     return 0;
+}
+
+int countKeys(){
+    int count = 0;
+    for (int i = 0; i < 4; i++){
+        if (key[i] == true){
+            count++;
+        }
+        if (count == 2){
+            break;
+        }
+    }
+    return count;
 }
 
 int hay_escaleras(int arriba){
-    if (xM > 720 && xM < 752) {
-        if (yM < 484.0 && arriba == 0) {
-            return 1;
-        } else if (yM > 426.0 && arriba == 1) {
+    //printf("x = %f ^ y = %f ^ piso = %f\n",xM,yM,nivelPiso);
+    if (xM > 716 && xM < 748) {
+        if ((yM < 484.0 && yM >= 426.0 && arriba == 0) || (yM > 426.0 && yM <= 484.0 && arriba == 1)) {
+            subiendo_escalera = 1;
             return 1;
         }
+        if ((yM < 332.0 && yM >= 270.0 && arriba == 0) || (yM > 270.0 && yM <= 332.0 && arriba == 1)) {
+            subiendo_escalera = 1;
+            return 1;
+        }
+        subiendo_escalera = 0;
         return 0;
     }
     else if(xM > 368 && xM < 400){
-        if (yM < 416.0 && arriba == 0) {
+        if ((yM < 416.0 && yM >= 342.0 && arriba == 0) || (yM > 342.0 && yM <= 416.0 && arriba == 1)) {
+            subiendo_escalera = 1;
             return 1;
         }
-        plataforma = 1;
-        if (yM > 342.0 && arriba == 1) {
-            return 1;
-        }
+        subiendo_escalera = 0;
         return 0;
     }
     else if(xM > 112 && xM < 144){
-        if (yM < 408.0 && arriba == 0) {
-            return 1;
-        } else if (yM > 350.0 && arriba == 1) {
+        if ((yM < 408.0 && yM >= 350.0 && arriba == 0) || (yM > 350.0 && yM <= 408.0 && arriba == 1)) {
+            subiendo_escalera = 1;
             return 1;
         }
+        subiendo_escalera = 0;
+        return 0;
+    }
+    else if(xM > 432 && xM < 464){
+        if ((yM < 340.0 && yM >= 262.0 && arriba == 0) || (yM > 262.0 && yM <= 340.0 && arriba == 1)) {
+            subiendo_escalera = 1;
+            return 1;
+        }
+        subiendo_escalera = 0;
         return 0;
     }
     return 0;
 }
 
-int obtenerPiso(int x){
+int obtenerPiso(float x){
     int nx = 0;
     int i = 0;
     int j;
@@ -266,7 +314,7 @@ int obtenerPiso(int x){
             i++;
         }
     }
-    else{
+    else if (nivelPiso > 316){
         nx = 28;
         j = -2;
         while (x > -16 + (i * 64)) {
@@ -278,72 +326,92 @@ int obtenerPiso(int x){
             i++;
         }
     }
+    else{
+        nx = 46;
+        j = 11;
+        while (x > -16 + (i * 64)) {
+            if (x <= -16 + ((i + 1) * 64)) {
+                nx += 2 * j;
+                break;
+            }
+            j--;
+            i++;
+        }
+    }
     return nx;
 }
 
-void escaleras(ALLEGRO_BITMAP  *image){
-    al_draw_bitmap(image, 736, 487, 0);
-    al_draw_bitmap(image, 736, 472, 0);
-    al_draw_bitmap(image, 320, 510, 0);
-    al_draw_bitmap(image, 320, 446, 0);
+void escaleras(ALLEGRO_BITMAP  *imagen){
+    al_draw_bitmap(imagen, 736, 487, 0);
+    al_draw_bitmap(imagen, 736, 472, 0);
+    al_draw_bitmap(imagen, 320, 510, 0);
+    al_draw_bitmap(imagen, 320, 446, 0);
 
-    al_draw_bitmap(image, 128, 397, 0);
-    al_draw_bitmap(image, 128, 412, 0);
-    al_draw_bitmap(image, 384, 390, 0);
-    al_draw_bitmap(image, 384, 405, 0);
-    al_draw_bitmap(image, 384, 420, 0);
+    al_draw_bitmap(imagen, 128, 397, 0);
+    al_draw_bitmap(imagen, 128, 412, 0);
+    al_draw_bitmap(imagen, 384, 390, 0);
+    al_draw_bitmap(imagen, 384, 405, 0);
+    al_draw_bitmap(imagen, 384, 420, 0);
+
+    al_draw_bitmap(imagen, 256, 356, 0);
+    al_draw_bitmap(imagen, 256, 294, 0);
+    al_draw_bitmap(imagen, 448, 340, 0);
+    al_draw_bitmap(imagen, 448, 325, 0);
+    al_draw_bitmap(imagen, 448, 310, 0);
+    al_draw_bitmap(imagen, 736, 332, 0);
+    al_draw_bitmap(imagen, 736, 317, 0);
 }
 
-void platforms(ALLEGRO_BITMAP  *image){
+void platforms(ALLEGRO_BITMAP  *imagen){
     int i;
     for (i = 0; i < 14; i++) {
-        al_draw_bitmap(image, i * 32, 519, 0);
+        al_draw_bitmap(imagen, i * 32, 519, 0);
     }
     int j = 2;
     for (; i < 28; i += 2) {
-        al_draw_bitmap(image, i * 32, 519-j, 0);
-        al_draw_bitmap(image, (i+1) * 32, 519-j, 0);
+        al_draw_bitmap(imagen, i * 32, 519-j, 0);
+        al_draw_bitmap(imagen, (i+1) * 32, 519-j, 0);
         j += 2;
     }
     j = 26;
     for (i = 0; i < 26; i += 2) {
-        al_draw_bitmap(image, i * 32, 455-j, 0);
-        al_draw_bitmap(image, (i+1) * 32, 455-j, 0);
+        al_draw_bitmap(imagen, i * 32, 455-j, 0);
+        al_draw_bitmap(imagen, (i+1) * 32, 455-j, 0);
         j -= 2;
     }
     j = 0;
     for (i = 2; i < 28; i += 2) {
-        al_draw_bitmap(image, i * 32, 377-j, 0);
-        al_draw_bitmap(image, (i+1) * 32, 377-j, 0);
+        al_draw_bitmap(imagen, i * 32, 377-j, 0);
+        al_draw_bitmap(imagen, (i+1) * 32, 377-j, 0);
         j += 2;
     }
     j = 26;
     for (i = 0; i < 26; i += 2) {
-        al_draw_bitmap(image, i * 32, 299-j, 0);
-        al_draw_bitmap(image, (i+1) * 32, 299-j, 0);
+        al_draw_bitmap(imagen, i * 32, 299-j, 0);
+        al_draw_bitmap(imagen, (i+1) * 32, 299-j, 0);
         j -= 2;
     }
     j = 0;
     for (i = 2; i < 28; i += 2) {
-        al_draw_bitmap(image, i * 32, 221-j, 0);
-        al_draw_bitmap(image, (i+1) * 32, 221-j, 0);
+        al_draw_bitmap(imagen, i * 32, 221-j, 0);
+        al_draw_bitmap(imagen, (i+1) * 32, 221-j, 0);
         j += 2;
     }
     j = 12;
     for (i = 14; i < 26; i += 2) {
-        al_draw_bitmap(image, i * 32, 143-j, 0);
-        al_draw_bitmap(image, (i+1) * 32, 143-j, 0);
+        al_draw_bitmap(imagen, i * 32, 143-j, 0);
+        al_draw_bitmap(imagen, (i+1) * 32, 143-j, 0);
         j -= 2;
     }
     for (i = 0; i < 14; i++) {
-        al_draw_bitmap(image, i * 32, 129, 0);
+        al_draw_bitmap(imagen, i * 32, 129, 0);
     }
     for (i = 11; i < 17; i++) {
-        al_draw_bitmap(image, i * 32, 65, 0);
+        al_draw_bitmap(imagen, i * 32, 65, 0);
     }
 }
 
-void barrels(ALLEGRO_BITMAP  *image){
+void barrels(ALLEGRO_BITMAP  *imagen){
     int i;
     int j = 0;
     for (i = 0; j < 2; i++) {
@@ -352,18 +420,18 @@ void barrels(ALLEGRO_BITMAP  *image){
             i = -1;
         }
         else {
-            al_draw_bitmap(image, 20 * i - 6, 104 - 32 * j, 0);
+            al_draw_bitmap(imagen, 20 * i - 6, 104 - 32 * j, 0);
         }
     }
 }
 
-void martillos(ALLEGRO_BITMAP *martillo){
-    al_draw_bitmap(martillo, 64, 167, 0);
-    al_draw_bitmap(martillo, 800, 398, 0);
+void martillos(ALLEGRO_BITMAP *imagen){
+    al_draw_bitmap(imagen, 64, 167, 0);
+    al_draw_bitmap(imagen, 800, 398, 0);
 }
 
-void gasolina_inicial(ALLEGRO_BITMAP* gasolina){
-    al_draw_bitmap(gasolina, 64, 478, 0);
+void gasolina_inicial(ALLEGRO_BITMAP* imagen){
+    al_draw_bitmap(imagen, 64, 478, 0);
 }
 
 ALLEGRO_BITMAP* createImage(char* carpeta, char* nombreImagen){
--- GUI/window.h
+++ GUI/window.h
@@ -16,6 +16,7 @@ ALLEGRO_BITMAP* escalera;
 
 ALLEGRO_BITMAP* marioX[3];
 ALLEGRO_BITMAP* marioY[3];
+ALLEGRO_BITMAP* marioZ[2];
 float nivelPiso;
 
 bool key[4];
@@ -24,19 +25,21 @@ enum MYKEYS {
     KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
 };
 float salto;
-int plataforma;
+int subiendo_escalera;
+int countKeys();
+int saltando;
 
 int hay_escaleras(int arriba);
 DWORD WINAPI saltar();
-int obtenerPiso(int x);
+int obtenerPiso(float x);
 int initialize();
 void finishExecution();
 void reading();
-void escaleras(ALLEGRO_BITMAP  *image);
-void platforms(ALLEGRO_BITMAP  *image);
-void barrels(ALLEGRO_BITMAP  *image);
-void martillos(ALLEGRO_BITMAP *martillo);
-void gasolina_inicial(ALLEGRO_BITMAP* gasolina);
+void escaleras(ALLEGRO_BITMAP  *imagen);
+void platforms(ALLEGRO_BITMAP  *imagen);
+void barrels(ALLEGRO_BITMAP  *imagen);
+void martillos(ALLEGRO_BITMAP *imagen);
+void gasolina_inicial(ALLEGRO_BITMAP* imagen);
 ALLEGRO_BITMAP* createImage(char* carpeta, char* nombreImagen);
 
 #endif //DONCEYKONGUSER_WINDOW_H
